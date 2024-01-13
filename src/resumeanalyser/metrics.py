a661ba9e20344437029a28c42a24db542bd83aea
prabh34 <prabhjit.thind95@gmail.com>
2024-01-12 22:27:40 -0800

    metrics skeleton update 2

--- src/resumeanalyser/metrics.py
+++ src/resumeanalyser/metrics.py
@@ -17,19 +17,20 @@ def SimilarityCV(text1, text2):
     return
 
 
-def SimilaritySpacy(text1, text2):
+def SimilaritySpacy(text1, text2, model):
     """
     To calculate cosine-similarity score using Spacy embeddings between two strings of text
 
     Parameters:
     text1 (str): First string containing the text be compared. eg. Job Description
     text2 (str): Second string containing the text be compared. eg. Resume text
+    model: Spacy model object eg. en_core_web_md, en_core_web_lg
 
     Returns:
     float: Cosine-Similarity score which shows how semantically similar the two strings are.
 
     Example:
-    >>> SimilaritySpacy("I am studying Data Science at UBC", "There are many good sources to study Data Science online")
+    >>> SimilaritySpacy("I am studying Data Science at UBC", "There are many good sources to study Data Science online", spacy.load("en_core_web_md"))
     45.3
     """
 
