dd52f868294ffd07e4676f55cc6fb08e635ac2ed
gtmx23 <greteltan21@gmail.com>
2024-01-12 22:36:36 -0800

    Added function documentation and specifications for text reading

---
+++ src/resumeanalyser/text_reading.py
@@ -0,0 +1,50 @@
+def pdf_to_text(filepath):
+    """
+    Basic function to extract text from a PDF file, given a specified file path.
+
+    Parameters:
+    filepath (str): A string containing the filepath.
+
+    Returns:
+    text (str): A string containing the extracted text.
+
+    Example:
+    >>> pdf_to_text('~/alphabet.pdf')
+    'abcdefghijklmnopqrstuvwxyz'
+    """
+
+    return text
+
+def website_to_text(url):
+    """
+    Basic function to extract text from a website, given a URL.
+
+    Parameters:
+    url (str): A string containing the filepath.
+
+    Returns:
+    text (str): A string containing the extracted text.
+
+    Example:
+    >>> website_to_text('www.alphabet.com')
+    'abcdefghijklmnopqrstuvwxyz'
+    """
+
+    return text
+
+def docx_to_text(filepath):
+    """
+    Basic function to extract text from a Word document, given a specified file path.
+
+    Parameters:
+    filepath (str): A string containing the filepath.
+
+    Returns:
+    text (str): A string containing the extracted text.
+
+    Example:
+    >>> pdf_to_text('~/alphabet.docx')
+    'abcdefghijklmnopqrstuvwxyz'
+    """
+
+    return text
