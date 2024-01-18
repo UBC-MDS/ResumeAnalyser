ad3ba86b504628f4b5a19a9a65321dc5a18cbd54
gtmx23 <greteltan21@gmail.com>
2024-01-18 08:51:10 -0800

    Working on feat_extract_text

--- src/resumeanalyser/text_reading.py
+++ src/resumeanalyser/text_reading.py
@@ -1,3 +1,36 @@
+# Imports
+from docx import Document
+import os
+
+def docx_to_text(filepath):
+    """
+    Basic function to extract text from a Word document, given a specified file path.
+
+    Parameters:
+    filepath (str): A string containing the filepath.
+
+    Returns:
+    text (str): A string containing the extracted text.
+
+    Example:
+    >>> pdf_to_text('~/alphabet.docx')
+    'abcdefghijklmnopqrstuvwxyz'
+    """
+    # Check that file path ends with docx
+    if not filepath.lower().endswith('.docx'):
+        raise ValueError("Invalid file format. Please provide a .docx file.")
+    document = Document(filepath)
+    full_text = []
+
+    for paragraph in document.paragraphs:
+        full_text.append(paragraph.text)
+
+    return '\n'.join(full_text)
+
+# delete later - test
+test_text = docx_to_text('~/git/mds/524_DSCI/resumeanalyser/tests/data/Sample_florist_resume.docx')
+print(test_test)
+
 def pdf_to_text(filepath):
     """
     Basic function to extract text from a PDF file, given a specified file path.
@@ -32,19 +65,3 @@ def website_to_text(url):
 
     return text
 
-def docx_to_text(filepath):
-    """
-    Basic function to extract text from a Word document, given a specified file path.
-
-    Parameters:
-    filepath (str): A string containing the filepath.
-
-    Returns:
-    text (str): A string containing the extracted text.
-
-    Example:
-    >>> pdf_to_text('~/alphabet.docx')
-    'abcdefghijklmnopqrstuvwxyz'
-    """
-
-    return text
