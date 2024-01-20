4c8b156aaaa0b65cd9209b6c4bf31323ee73e44c
prabh34 <prabhjit.thind95@gmail.com>
2024-01-19 16:41:24 -0800

    metrics code and test cases

--- src/resumeanalyser/metrics.py
+++ src/resumeanalyser/metrics.py
@@ -1,3 +1,7 @@
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import spacy
+
 def SimilarityCV(text1, text2):
     """
     To calculate cosine-similarity score using CountVectorizer between two strings of text
@@ -13,11 +17,23 @@ def SimilarityCV(text1, text2):
     >>> SimilarityCV("I am studying Data Science at UBC", "There are many good sources to study Data Science online")
     21.8
     """
+    if not text1 or not text2:
+        raise ValueError("Both input strings must not be empty.")
+    
+    if not isinstance(text1, str) or not isinstance(text2, str):
+        raise ValueError("Both inputs must be strings.")
 
-    return
+    cv = CountVectorizer()
+    text = [text1, text2]
+    count_matrix = cv.fit_transform(text)
 
+    similarityScore = round(cosine_similarity(count_matrix)[0][1] * 100, 2)
 
-def SimilaritySpacy(text1, text2, model):
+    return similarityScore
+
+#print(SimilarityCV("I am studying Data Science at UBC", "There are many good sources to study Data Science online"))
+
+def SimilaritySpacy(text1, text2):
     """
     To calculate cosine-similarity score using Spacy embeddings between two strings of text
 
@@ -30,8 +46,27 @@ def SimilaritySpacy(text1, text2, model):
     float: Cosine-Similarity score which shows how semantically similar the two strings are.
 
     Example:
-    >>> SimilaritySpacy("I am studying Data Science at UBC", "There are many good sources to study Data Science online", spacy.load("en_core_web_md"))
+    >>> SimilaritySpacy("I am studying Data Science at UBC", "There are many good sources to study Data Science online")
     45.3
     """
+    if not text1 or not text2:
+        raise ValueError("Both input strings must not be empty.")
+        
+    if not isinstance(text1, str) or not isinstance(text2, str):
+        raise ValueError("Both inputs must be strings.")
 
-    return
+    if "en_core_web_md" not in spacy.cli.info():
+        print("Downloading 'en_core_web_md' model...")
+        spacy.cli.download("en_core_web_md")
+    # Load the NLP model
+    nlp = spacy.load("en_core_web_md")
+
+    text1 = nlp(text1)
+    text2 = nlp(text2)
+
+    # Calculating the similarity between the two texts
+    similarity_score = text1.similarity(text2)
+
+    return similarity_score
+
+#print(SimilaritySpacy("I am studying Data Science at UBC", "There are many good sources to study Data Science online"))
